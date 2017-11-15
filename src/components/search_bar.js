910ba19e027121376d208de41d61560d8ee9999e
David Saunders <david@monkee-boy.com>
2017-11-15 13:43:03 -0600

    Added onChange method to SearchBar input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,7 @@ import React, { Component } from 'react';
 // Class component - Used when we want a component to have internal record keeping
 class SearchBar extends Component { // Declares new class with name 'SearchBar'; Class has access to functionality that React.Component class has
   render() { // Define render method on the class -- every class must have this
-    return <input />; // Must return JSX
+    return <input onChange={event => console.log(event.target.value)} />; // Must return JSX; onChange is a react-specific property
   }
 }
 
