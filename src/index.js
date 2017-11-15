2ca75123e23328f452cdbed08bc6b32aa6b4895c
David Saunders <david@monkee-boy.com>
2017-11-15 09:14:07 -0600

    Created individual component files for video player app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML. 
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Put component's generated HTML and put it in the DOM. First argument is the instance of the class. Second argument is the placement location. 
+ReactDOM.render(<App />, document.querySelector('.container'));
