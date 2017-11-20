d27ab58e09d30e4773d74c524743877fbafa013c
David Saunders <david@monkee-boy.com>
2017-11-20 09:17:42 -0800

    Convert main app component from functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = "AIzaSyAYgpph-RNZFJbBv1C_nnqOSi6Z3M4Blq8";
 
+// Downwards data flow - only the most parent component in an application should be responsible for fetching data
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 // Create a new component. This component should produce some HTML. 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Put component's generated HTML and put it in the DOM. First argument is the instance of the class. Second argument is the placement location. 
