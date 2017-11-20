a81432f59afbbeb682b3354a61d6eb3f4325e2ef
David Saunders <david@monkee-boy.com>
2017-11-20 09:22:53 -0800

    Add YouTube search functionality to app constructor function

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = "AIzaSyAYgpph-RNZFJbBv1C_nnqOSi6Z3M4Blq8";
 
-// Downwards data flow - only the most parent component in an application should be responsible for fetching data
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 // Create a new component. This component should produce some HTML. 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // Downwards data flow - only the most parent component in an application should be responsible for fetching data
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
