57ed5d5c5a8469576d754fe6d4515db61d38a789
David Saunders <david@monkee-boy.com>
2017-11-27 12:35:24 -0600

    Set up initial video list item structure and loop through videos array

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // No need for state - doesn't record user interaction or rerender itself, so it can be a plain, functional component
-const VideoList = () => {
+// Include 'props' in function creation to pull in props passed to component within the main App
+// In a class-based component, props are available anywhere, in any defined method, as this.props. Props are available as just "props" in a function-based component
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group"> 
+      {videoItems}
     </ul>
   );
 };
