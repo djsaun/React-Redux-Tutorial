65632bacee71f6710477a6aa5c012cdeb668f44c
David Saunders <david@monkee-boy.com>
2017-11-27 09:48:58 -0600

    Created VideoList component and added it to main App

---
+++ src/components/video_list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// No need for state - doesn't record user interaction or rerender itself, so it can be a plain, functional component
+const VideoList = () => {
+  return (
+    <ul className="col-md-4 list-group"> 
+    </ul>
+  );
+};
+
+export default VideoList;
+
+
